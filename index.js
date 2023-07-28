bc08522e3d8df85bdc008140a5b5e8cb6d56eb38
mouniabelkheir <mouniabelkheir@outlook.com>
2023-07-28 23:56:45 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+// writeCards function implementation
+function writeCards(names, occasion) {
+    return names.map((name) => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+  
+  // countDown function implementation
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
